660fec6090d42f29012178698bf7d9cc22f06e74
Elena-Weber <crochetmiracles@gmail.com>
2022-10-14 17:57:34 -0400

    add styling and icons

--- App.js
+++ App.js
@@ -5,21 +5,52 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AllExpenses from './screens/All';
 import RecentExpenses from './screens/Recent';
 import ManageExpenses from './screens/Manage';
+import { Styles } from './constants/styles';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function Overview() {
-  return <BottomTabs.Navigator>
-    <BottomTabs.Screen
-      component={RecentExpenses}
-      name="Recent Expenses"
-    />
-    <BottomTabs.Screen
-      component={AllExpenses}
-      name="All Expenses"
-    />
-  </BottomTabs.Navigator>
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: Styles.colors.primaryDark},
+        headerTintColor: 'white',
+        tabBarStyle: {backgroundColor: Styles.colors.primaryDark},
+        tabBarActiveTintColor: 'white'
+      }}
+    >
+      <BottomTabs.Screen
+        component={RecentExpenses}
+        name="Recent Expenses" // name in header
+        options={{
+          // title: "Recent Expenses",
+          tabBarLabel: 'Recent', // name in tab bar
+          tabBarIcon: ({color, size}) =>
+            <Ionicons
+              name="hourglass"
+              size={size}
+              color={color}
+            />
+        }}
+      />
+      <BottomTabs.Screen
+        component={AllExpenses}
+        name="All Expenses"
+        options={{
+          // title: "All Expenses",
+          tabBarLabel: 'All', // name in tab bar
+          tabBarIcon: ({color, size}) =>
+            <Ionicons
+              name="calendar"
+              size={size}
+              color={color}
+            />
+        }}
+      />
+    </BottomTabs.Navigator>
+  )
 }
 
 export default function App() {
@@ -31,6 +62,7 @@ export default function App() {
           <Stack.Screen
             component={Overview}
             name="Overview"
+            options={{headerShown: false}}
           />
           <Stack.Screen
             component={ManageExpenses}
---
+++ constants/styles.js
@@ -0,0 +1,9 @@
+export const Styles = {
+    colors: {
+        primary: '#e4d9fd',
+        primaryLight: '#c6affc',
+        primaryDark: '#200364',
+        accent: '#f7bc0c',
+        error: '#9b095c'
+    }
+}
