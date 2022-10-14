996198c924dc6921d4c7c45a768f272a77a9ac60
Elena-Weber <crochetmiracles@gmail.com>
2022-10-14 16:48:04 -0400

    add navigation

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AllExpenses from './screens/All';
+import RecentExpenses from './screens/Recent';
+import ManageExpenses from './screens/Manage';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function Overview() {
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen
+      component={RecentExpenses}
+      name="Recent Expenses"
+    />
+    <BottomTabs.Screen
+      component={AllExpenses}
+      name="All Expenses"
+    />
+  </BottomTabs.Navigator>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            component={Overview}
+            name="Overview"
+          />
+          <Stack.Screen
+            component={ManageExpenses}
+            name="Manage Expenses"
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
